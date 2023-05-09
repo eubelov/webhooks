6d25b8ea1fc4ac823227616a2a572931e94681a9
Evgeny Belov <evgeny.belov@webjet.com.au>
2023-05-10 09:48:36 +1000

    Save

--- src/Webhooks.Host/Program.cs
+++ src/Webhooks.Host/Program.cs
@@ -10,21 +10,8 @@ builder.Host.UseSerilog((ctx, lc) =>
     lc.Enrich.WithCorrelationId();
 });
 
-
 builder.Services.RegisterEngineModuleDependencies(builder.Configuration);
 builder.Services.RegisterBwModuleDependencies(builder.Configuration);
 
 var app = builder.Build();
-var sp = app.Services.CreateScope()
-    .ServiceProvider
-    .GetRequiredService<IPublishEndpoint>();
-
-for (int i = 0; i < 1; i++)
-{
-    await sp.Publish(new NotifyCreated("333333333", "79117984018"),
-        x => x.CorrelationId = Guid.NewGuid());
-    await sp.Publish(new NotifyModerationCompleted("333333333", "79117984018"),
-        x => x.CorrelationId = Guid.NewGuid());
-}
-
 app.Run();
